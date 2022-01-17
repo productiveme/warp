e2ec8cc1d0237e9d19ec98a71489ca0412b84b2e
Jaco Swarts <jaswarts@ebay.com>
2022-01-17 15:55:44 +0200

    WIP - adding people

---
+++ .deploy/mup.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+module.exports = {
+  servers: {
+    one: {
+      host: 'ec2-18-206-89-180.compute-1.amazonaws.com',
+      username: 'ubuntu',
+      pem: './gumtree.pem'
+    }
+  },
+
+  app: {
+    name: 'warp',
+    path: '../',
+    type: 'docker-image',
+
+    servers: {
+      one: {},
+    },
+
+    env: {
+      PORT: 5000,
+      SECRET_KEY: process.env.SECRET_KEY,
+    },
+
+    docker: {
+      imagePort: 5000,
+    },
+
+  },
+
+  // (Optional)
+  // Use the proxy to setup ssl or to route requests to the correct
+  // app when there are several apps
+  plugins: ['mup-docker-deploy'],
+
+  proxy: {
+    domains: 'ctoffice.gumtree.co.za',
+
+    ssl: {
+      // Enable Let's Encrypt
+      letsEncryptEmail: 'jaswarts@eclassifiedsgroup.com'
+    }
+  }
+};
--- warp/sql/sample_data.sql
+++ warp/sql/sample_data.sql
@@ -1,8 +1,59 @@
 
 INSERT INTO users VALUES (1,'admin','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (2,'bswart','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Barend Hermanus Swart',1);
+INSERT INTO users VALUES (3,'ccobbledick','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (4,'chvermeulen','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (5,'csass','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (6,'dcloete','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (7,'bwevers','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (8,'jaswarts','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (9,'jgaud','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (10,'lbeyers','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (11,'lkannan','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (12,'pvimbayo','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (13,'rdukejackson','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (14,'rvanderwesthuizen','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (15,'tmahlaola','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (16,'zjansenvanvuuren','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (17,'enagel','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (18,'ijoubert','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (19,'jaswarts','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (20,'jduplessis','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (21,'jhelme','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (22,'mbadach','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (23,'michaewalker','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (24,'nudixon','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+INSERT INTO users VALUES (25,'stakurpersad','pbkdf2:sha256:260000$UEV4fnkO1Mtr5EBq$4624e65fae621ec38b6e6c47f49642a120dd91e2dc8c10477b5128b8de4b14dc','Admin',1);
+
+-- Barend Hermanus Swart <bswart@eclassifiedsgroup.com>
+-- ccobbledick                                Cobbledick, Claire             
+-- chvermeulen                                Vermeulen, Chris(AWF)          
+-- csass                                       Sass, Cheryl(AWF)              
+-- dcloete                                       Cloete, Daniel(AWF)            
+-- bwevers                                       Wevers, Burton(AWF)            
+-- jaswarts                                      Swarts, Jaco                   
+-- jgaud                                Gaud, JM(AWF)                  
+-- lbeyers                                Beyers, Lodes(AWF)             
+-- lkannan                                Kannan, Lakshmi(AWF)           
+-- pvimbayo                               Vimbayo, Phiwo(AWF)            
+-- rdukejackson                               Duke-Jackson, Rudi(AWF)        
+-- rvanderwesthuizen                                Van Der Westhuizen, Ryan(AWF)  
+-- tmahlaola                                Mahlaola, Thamsanqa(AWF)       
+-- zjansenvanvuuren                               Jansen Van Vuuren, Zandre      
+-- enagel                               Nagel, Estelle                 
+-- gwessmann                                Wessmann, Goltz(AWF)           
+-- ijoubert                               Joubert, Izak(AWF)             
+-- jaswarts                               Swarts, Jaco                   
+-- jduplessis                               Du Plessis, Jaco(AWF)          
+-- jhelme                               Helme, Jake(AWF)               
+-- mbadach                                Badach, Marcus(AWF)            
+-- michaewalker                               Walker, Michael                
+-- nudixon                                Dixon, Nunben                  
+-- stakurpersad                               Takurpersad, Shalla(AWF)        
 
 INSERT INTO zone VALUES (1,1,'Office','zone_maps/office.png');
 INSERT INTO zone VALUES (2,2,'Parking','zone_maps/parking.png');
+
 
 INSERT INTO seat (zid,name,x,y) VALUES (1,'Desk 1' ,216,36);
 INSERT INTO seat (zid,name,x,y) VALUES (1,'Desk 2' ,216,105);
